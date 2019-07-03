7a670d372e8b48bbf2ccf370bb22fe2ca6c8b99a
e1fang <438283787@qq.com>
2019-07-03 17:05:00 +0800

    add

---
+++ 豆瓣电影/js/copy-1.js
@@ -0,0 +1,25 @@
+var top250 ={
+    init:function(){
+        this.bind()
+    },
+    bind:function(){
+    
+    }
+}
+
+
+
+
+
+var app = {
+    init:function() {
+        this.bind();
+    },
+    bind:function(){
+        var _this = this;
+        this.$tabs.on('click' , function(){})
+
+    }
+}
+
+app.init();
